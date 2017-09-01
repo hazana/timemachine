23e601be4a96a4c36079385b4fcefceeda4c4267
Alex W <git@alexwh.com>
2017-09-01 17:32:28 +0100

    convert to python3 string formatting

--- waybackrobots.py
+++ waybackrobots.py
@@ -9,7 +9,7 @@ from multiprocessing.dummy import Pool
 def robots(host):
     r = requests.get(
         'https://web.archive.org/cdx/search/cdx\
-        ?url=%s/robots.txt&output=json&fl=timestamp,original&filter=statuscode:200&collapse=digest' % host)
+        ?url={}/robots.txt&output=json&fl=timestamp,original&filter=statuscode:200&collapse=digest'.format(host))
     results = r.json()
     if len(results) == 0: # might find nothing
         return []
@@ -18,7 +18,7 @@ def robots(host):
 
 
 def getpaths(snapshot):
-    url = 'https://web.archive.org/web/{0}/{1}'.format(snapshot[0], snapshot[1])
+    url = 'https://web.archive.org/web/{0}/{1}'.format(*snapshot)
     robotstext = requests.get(url).text
     if 'Disallow:' in robotstext:  # verify it's acually a robots.txt file, not 404 page
         paths = re.findall('/.*', robotstext)
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     snapshots = robots(args.host)
-    print('Found %s unique results' % len(snapshots))
+    print('Found {} unique results'.format(len(snapshots)))
     if len(snapshots) == 0:
         sys.exit()
     print('This may take some time...')
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     unique_paths = set()
     for i in paths:
         unique_paths.update(i)
-    filename = '%s-robots.txt' % args.host
+    filename = '{}-robots.txt'.format(args.host)
     with open(filename, 'w') as f:
         f.write('\n'.join(unique_paths))
-    print('[*] Saved results to %s' % filename)
+    print('[*] Saved results to {}'.format(filename))

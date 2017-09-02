c0426b2a7557ca27408adbbc424b93706665f911
Alex W <git@alexwh.com>
2017-09-02 15:20:16 +0100

    merge python scripts to one file

--- waybackrobots.py
+++ waybackrobots.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import re
+import json
 import argparse
 import requests
 from multiprocessing.dummy import Pool
@@ -25,11 +26,27 @@ def getpaths(snapshot):
         return paths
     return []
 
+def waybackurls(host, with_subs):
+    url = 'http://web.archive.org/cdx/search/cdx?url={glob}{host}/*&output=json&fl=original&collapse=urlkey'.format(glob='*.' if with_subs else '', host=host)
+    r = requests.get(url)
+    results = r.json()
+    return results[1:]
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("host", help="host to scan")
+    parser.add_argument("--with-subs", help="scan url's subdomains also", action="store_true")
     args = parser.parse_args()
+
+    urls = waybackurls(args.host, args.with_subs)
+    json_urls = json.dumps(urls)
+    if urls:
+        filename = '{}-waybackurls.json'.format(args.host)
+        with open(filename, 'w') as f:
+            f.write(json_urls)
+        print('[*] Saved results to {}'.format(filename))
+    else:
+        print('[-] Found nothing')
 
     snapshots = robots(args.host)
     print('Found {} unique results'.format(len(snapshots)))
